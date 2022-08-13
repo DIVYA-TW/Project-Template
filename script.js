e681a1cb4f23a446ebc00b71bef00facfadd6519
DIVYA-TW <sharmad9654@gmail.com>
2022-08-13 17:46:04 +0530

    This is an initial commit of version 1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+var menu=document.getElementById('menubar');
+
+function menudisplay(){
+    if(menu.style.maxHeight=="0vh"){
+        menu.style.maxHeight="45vh";
+    }
+    
+    else{
+        menu.style.maxHeight="0vh";
+    }
+}
+
